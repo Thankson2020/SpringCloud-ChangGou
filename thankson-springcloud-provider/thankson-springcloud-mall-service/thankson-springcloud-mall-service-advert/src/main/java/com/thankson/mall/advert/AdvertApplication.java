0df78b61022a217a1fcf5a58fa54e07bf8b79bab
Thankson <693039703@qq.com>
2020-06-17 22:47:11 +0800

    canal同步广告

--- thankson-springcloud-provider/thankson-springcloud-mall-service/thankson-springcloud-mall-service-advert/src/main/java/com/thankson/mall/advert/AdvertApplication.java
+++ thankson-springcloud-provider/thankson-springcloud-mall-service/thankson-springcloud-mall-service-advert/src/main/java/com/thankson/mall/advert/AdvertApplication.java
@@ -3,13 +3,11 @@ package com.thankson.mall.advert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.thankson.mall.advert.dao"})
-@EnableFeignClients
 public class AdvertApplication {
 
     public static void main(String[] args) {
